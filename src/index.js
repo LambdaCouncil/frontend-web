6c187b79db9136fc7d3cced210110b75a014d532
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-10-08 13:25:58 -0700

    added redirect to index if user is already logged in.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.css';
 import App from "./components/App";
 import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
 
 
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-);
+function Root(props) {
+    useEffect(() => 
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                props.history.push("/");
+            }
+        })
+    );
+        return (
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+        );
+} 
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RouteWithAuth = withRouter(Root);
+
+ReactDOM.render(
+<Router>
+    <RouteWithAuth />
+</Router>, 
+document.getElementById('root'));
 registerServiceWorker();
