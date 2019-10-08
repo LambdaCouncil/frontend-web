22206c9c47022cefe6961def30bb27bb3059b5ad
Greg Johnston <GCJohnston91@gmail.com>
2019-10-08 12:07:26 -0400

    Loading on submit button. Full form validation

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -8,6 +8,8 @@ function Register() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [errors, setErrors] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const handleChangeUser = (e) => {
        setUserName(e.target.value);
@@ -26,17 +28,55 @@ function Register() {
         console.log('passwordConfirm', passwordConfirm)
     };
 
-    const handleSubmit = (e) =>{
+    const isFormValid = () => {
+        let error;
+        if(isFormEmpty(userName, email, password, passwordConfirm)){
+            error = {message: "Moroni needs all fields"};
+            setErrors((errors => errors.concat(error)));
+            return false;
+        } else if (!isPasswordValid(password, passwordConfirm)){
+            error = {message: 'Password not Joseph Smith. Why?'};
+            setErrors((errors => errors.concat(error)));
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    const isFormEmpty = (userName, email, password, passwordConfirm) => {
+        return !userName.length || !email.length || !password.length || !passwordConfirm.length;
+    };
+
+    const isPasswordValid = (password, passwordConfirm) => {
+        if(password.length < 6 || passwordConfirm.length < 6) {
+            return false;
+        } else if (password !== passwordConfirm){
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    const displayErrors = (errors) => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+    const handleSubmit = (e) => {
         e.preventDefault();
+        if (isFormValid()) {
+            setErrors([]);
+            setLoading(true);
         firebase
             .auth()
             .createUserWithEmailAndPassword(email, password)
             .then(createdUser => {
-                console.log('createdUser', createdUser)
+                console.log('createdUser', createdUser);
+                setLoading(false);
             })
             .catch(err => {
-                console.log(err)
+                console.log(err);
+                setLoading(false);
+                setErrors((errors => errors.concat(err)));
             })
+    }
     };
 
     return (
@@ -80,13 +120,18 @@ function Register() {
                                     onChange={handleChangePasswordConfirm}
                                     value={passwordConfirm}
                                     type="password"/>
-                        <Button color="green" fluid size="large">Submit</Button>
+                        <Button disabled={loading} className = {loading ? 'loading' : ''}
+                            color="green" fluid size="large">Submit</Button>
                     </Segment>
                 </Form>
-                <Message>Already wearing the undapants? <Link to="/login">Login</Link></Message>
-
+                {errors.length > 0 && (
+                    <Message error>
+                        <h3>You have unconverted</h3>
+                        {displayErrors(errors)}
+                    </Message>
+                )}
+                <Message>Why are you wearing undapants? <Link to="/login">Login</Link></Message>
             </Grid.Column>
-
         </Grid>
     )
 }
