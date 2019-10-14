93308c03080daa89dd8b9ead9bbb3a8448642613
Greg Johnston <GCJohnston91@gmail.com>
2019-10-14 15:39:43 -0400

    Users for DMs built out. Need to fix async render.

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -6,15 +6,64 @@ const DirectMessages = ({currentUser}) => {
 
   const [users, setUsers] = useState([]);
   const user = currentUser;
+  const usersRef = firebase.database().ref('users');
+  const connectedRef = firebase.database().ref('.info/connected');
+  const presenceRef = firebase.database().ref('presence');
   console.log('user', user);
 
-  // useEffect(() => {
-  //   addListeners(user.uid)
-  // }, []);
+  useEffect(() => {
+    if (user) {
+      addListeners(user.uid)
+    }
+  }, []);
 
-  // const addListeners = currentUserUid => {
-  //
-  // };
+
+  const addListeners = currentUserUid => {
+    let loadedUsers = [];
+    usersRef.on('child_added', snap => {
+      if (currentUserUid !== snap.key) {
+        let user = snap.val();
+        user['uid'] = snap.key;
+        user['status'] = 'offline';
+        loadedUsers.push(user);
+        setUsers(loadedUsers);
+      }
+    });
+    connectedRef.on('value', snap => {
+      if (snap.val() === true) {
+        const ref = presenceRef.child(currentUserUid);
+        ref.set(true);
+        ref.onDisconnect().remove(err => {
+          if (err !== null) {
+            console.log(err);
+          }
+        })
+      }
+    });
+    presenceRef.on('child_added', snap => {
+      if (currentUserUid !== snap.key) {
+        addStatusToUser(snap.key)
+      }
+    });
+    presenceRef.on('child_removed', snap => {
+      if (currentUserUid !== snap.key) {
+        addStatusToUser(snap.key, false)
+      }
+    })
+  };
+
+  const addStatusToUser = (userId, connected = true) => {
+    const updatedUsers = users.reduce((acc, user) => {
+      if (user.uid === userId) {
+        user['status'] = `${connected ? 'online' : 'offline'}`;
+        return acc.concat(user);
+      }
+    }, []);
+    setUsers(updatedUsers)
+  };
+
+
+  const isUserOnline = user => user.status === 'online';
 
   return (
     <Menu.Menu className='menu'>
@@ -24,6 +73,19 @@ const DirectMessages = ({currentUser}) => {
         </span>{' '}
         ({ users.length })
       </Menu.Item>
+
+      {users.map(user => (
+        <Menu.Item
+        key={user.uid}
+        onClick={() => console.log(user)}
+        style={{ opacity: .07, fontStyle: 'italic' }}>
+          <Icon
+          name='circle'
+          color={isUserOnline(user) ? 'green' : 'red'}
+          />
+          @ {user.name}
+        </Menu.Item>
+      ))}
     </Menu.Menu>
   );
 };
